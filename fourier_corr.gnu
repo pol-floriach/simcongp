642a1917d6b58aeb5df55346979ea252eda656e9
pol <polfloridt@gmail.com>
2023-09-16 20:41:17 +0200

    Canviat p1c.f i fourier_corr.png

--- fourier_corr.gnu
+++ fourier_corr.gnu
@@ -1,10 +1,22 @@
-set terminal pngcairo enhanced font 'Verdana,12'
-set terminal png size 1024,768
+set terminal pngcairo
+#set terminal png size 1024,768
 
 set output 'fourier_corr.png'
 
+#TICS i BORDERS
+set tics nomirror
+set tics scale 0.7
 
-set xlabel '($\omega)'
-set ylabel '$S(\omega)'
+set border 3 lw 1.3
 
-plot 's_file.dat' with lines
+set grid
+
+#LABELS
+
+set xlabel '{/Symbol w}'
+set ylabel 'S({/Symbol w})'    
+
+unset key
+
+#PLOT EN SI
+plot 's_file.dat' w lines lc 3 lw 2
--- p1b.f
+++ p1b.f
@@ -1,13 +1,10 @@
-       program elfreakingpi
-       integer :: k_max = 1E5
-       integer :: k = 0.0
-       integer :: k_final
-       real :: pi_sum = 0.0
-       real :: pi_real = 3.141592
+       program leibniz_pi
+       integer :: k_max = 1E5, k = 0.0
+       real :: pi_sum = 0.0, pi_real = 3.141592
        
        open(11,file="pi_k.txt")
        
-    	do while (abs(pi_real - 4*pi_sum) .ge. 1d-6)
+    	do while (abs(pi_real - 4*pi_sum) .ge. 1e-6)
     		pi_sum = pi_sum + ((-1.0)**dfloat(k) / (2.0*dfloat(k)+1.0))
 			write(11,*) pi_sum*4
     		k = k+1
@@ -16,5 +13,5 @@
     		print *, "Valor final: ", pi_sum*4
 
        stop
-       end program elfreakingpi
+       end program leibniz_pi
        
